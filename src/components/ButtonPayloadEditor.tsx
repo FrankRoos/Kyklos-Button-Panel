8c17ee592eaa8366559ce6fb6cdf14f1fae1973c
FrankRoos <ftfe@gft.com>
2023-08-16 16:36:03 +0200

    modify payload editor file

--- src/components/ButtonPayloadEditor.tsx
+++ src/components/ButtonPayloadEditor.tsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import { StandardEditorProps } from '@grafana/data';
 import { Field, TextArea } from '@grafana/ui';
 
-import { ButtonPanelOptions, EditorCodeType } from 'types';
+import { ButtonPanelOptions, EditorCodeType} from 'types';
 
 interface Props extends StandardEditorProps<EditorCodeType, ButtonPanelOptions> {}
 
 export const ButtonPayloadEditor: React.FC<Props> = ({ item, value, onChange, context }) => {
-  const [newParamValue, setNewParamValue] = useState('');
+  const [newPayload, setNewPayload] = useState('');
 
-  const onNewParamValueChanged = (value: string) => {
-    setNewParamValue(value);
+  const onNewPayloadChanged = (value: string) => {
+    setNewPayload(value);
     onChange(value);
   };
 
@@ -18,10 +18,10 @@ export const ButtonPayloadEditor: React.FC<Props> = ({ item, value, onChange, co
     <div className="panel-container" style={{ width: 'auto' }}>
       <Field>
         <TextArea
-          value={newParamValue}
-          placeholder="{ query: 'your query' }"
+          value={newPayload}
+          placeholder="{ 'key': 'value' }"
           rows={5}
-          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => onNewParamValueChanged(e.target.value)}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => onNewPayloadChanged(e.target.value)}
         />
       </Field>
     </div>
